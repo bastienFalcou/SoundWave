0167f9781ad05f376209f70edb89c457bf51103b
ven1xus <bojanint@gmail.com>
2018-10-15 14:50:30 -0400

    converted ChronoMeter line that causes errors in swift 4 projects to swift 4 syntax

--- SoundWave/Classes/Chronometer.swift
+++ SoundWave/Classes/Chronometer.swift
@@ -25,7 +25,7 @@ public final class Chronometer: NSObject {
 
 	public func start(shouldFire fire: Bool = true) {
 		self.timer = Timer(timeInterval: self.timeInterval, target: self, selector: #selector(Chronometer.timerDidTrigger), userInfo: nil, repeats: true)
-		RunLoop.main.add(self.timer!, forMode: RunLoopMode.defaultRunLoopMode)
+		RunLoop.main.add(self.timer!, forMode: RunLoop.Mode.default)
 		self.timer?.fire()
 		self.isPlaying = true
 	}
