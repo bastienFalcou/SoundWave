f0c5a1a675438803f7475131a48ac460cf7eff52
Vladislav Prusakov <spectraldragonchannel@gmail.com>
2018-10-30 01:14:42 +0300

    Update to swift 4.2

--- Example/SoundWave/AppDelegate.swift
+++ Example/SoundWave/AppDelegate.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 @UIApplicationMain
-
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
 	var window: UIWindow?
-	
-	private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-		return true
-	}
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
 }
